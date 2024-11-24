22b0b2d7432f837dd7a313126b6cb0e4674c2cb9
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-11-24 10:07:28 +0900

    feat: 키보드 레이아웃 변경

--- src/Components/Common/Layouts.jsx
+++ src/Components/Common/Layouts.jsx
@@ -22,25 +22,28 @@ export const KoreanLayout = {
         "{lang} / {space} . {enter}"
     ],
     shift: [
-        "! @ # $ % ^ & * ( ) {bksp}",
-        "ㅃ ㅉ ㄸ ㄲ ㅆ ㅛ ㅕ ㅑ ㅒ ㅖ {lang}",
-        "ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ {enter}",
-        "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ {space}"
+        "! @ # $ % ^ & * ( )",
+        "ㅃ ㅉ ㄸ ㄲ ㅆ ㅛ ㅕ ㅑ ㅒ ㅖ",
+        "ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ",
+        "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ {bksp}",
+        "{lang} / {space} . {enter}"
     ],
 
 };
 
 export const EnglishLayout = {
     default: [
-        "1 2 3 4 5 6 7 8 9 0 {bksp}",
-        "q w e r t y u i o p {lang}",
-        "a s d f g h j k l {enter}",
-        "{shift} z x c v b n m {space}"
+        "1 2 3 4 5 6 7 8 9 0",
+        "q w e r t y u i o p",
+        "a s d f g h j k l",
+        "{shift} z x c v b n m {bksp}",
+        "{lang} / {space} . {enter}"
     ],
     shift: [
-        "! @ # $ % ^ & * ( ) _ + {bksp}",
-        "Q W E R T Y U I O P { } {lang}",
-        "A S D F G H J K L : \" {enter}",
-        "{shift} Z X C V B N M < > ? {space}"
+        "! @ # $ % ^ & * ( )",
+        "Q W E R T Y U I O P",
+        "A S D F G H J K L",
+        "{shift} Z X C V B N M {bksp}",
+        "{lang} / {space} . {enter}"
     ]
 };
